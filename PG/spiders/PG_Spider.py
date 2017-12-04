15331950c5fd30cf8c0841aa7fb3da624f519c86
Samuel Piecz <samuel.piecz@gmail.com>
2017-12-04 10:46:49 -0600

    Don't listen to robots.txt, extract url and name of article.

--- PG/spiders/PG_Spider.py
+++ PG/spiders/PG_Spider.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
+# response.xpath('//font/a/@href').extract()
 
 class PgSpiderSpider(scrapy.Spider):
     name = 'PG_Spider'
     allowed_domains = ['http://www.paulgraham.com/articles.html']
-    start_urls = ['http://http://www.paulgraham.com/articles.html/']
+    start_urls = ['http://paulgraham.com/articles.html/']
 
     def parse(self, response):
-        pass
+        article_link = website + response.xpath('//font/a/@href').extract()
+        article_name = response.xpath('//font/a/text()').extract()
+
+        yield {'Article Name': article_name, 'Article URL': article_link}
